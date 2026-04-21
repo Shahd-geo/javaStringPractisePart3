fbd2e4883a348a72cb4400819036a4b6306b605a
Shahd <shahad9mubarak@gmail.com>
2026-04-21 15:31:38 +0400

    doing loop

--- src/TASK4gHappy.java
+++ src/TASK4gHappy.java
@@ -1,13 +1,20 @@
 public class TASK4gHappy {
     static void main(String[] args) {
-
+        String str="xxggxx";
+        S
     }
+
+
     public static boolean gHappy(String str ){
         for (int i = 0 ; i<str.length();i++){
             if (str.charAt(i)=='g' &&str.charAt(i+1)=='g'||str.charAt(i-1)=='g'){
+                return true;
 
-            }
+            }else{
+            return false;
         }
+
     }
 
 }
+
