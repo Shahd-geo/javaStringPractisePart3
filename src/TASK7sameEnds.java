9971bda7798dba0ff4b398979bd59dc2e59741e2
Shahd <shahad9mubarak@gmail.com>
2026-04-21 16:20:00 +0400

    add int var

--- src/TASK7sameEnds.java
+++ src/TASK7sameEnds.java
@@ -1,5 +1,6 @@
 public class TASK7sameEnds {
     static void main(String[] args) {
         String sameEnds="abXYab";
-        for (int i = 0 ; i<sameEnds.length();i++){
+        int max = sameEnds.length()/2;
+        for
     }
