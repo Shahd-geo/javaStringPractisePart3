92facb2da63b492c0e36adfe66705606dcf4e1ab
Shahd <shahad9mubarak@gmail.com>
2026-04-21 15:26:06 +0400

    task 4

---
+++ src/TASK4gHappy.java
@@ -0,0 +1,11 @@
+public class TASK4gHappy {
+    static void main(String[] args) {
+
+    }
+    public static boolean gHappy(String str ){
+        for (int i = 0 ; i<str.length();i++){
+            if (str.charAt(i)=="g")
+        }
+    }
+
+}
