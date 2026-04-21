6cd6cbfd562499a58776b657f5085b6caeb01f77
Shahd <shahad9mubarak@gmail.com>
2026-04-21 15:11:34 +0400

    add if is founf

--- src/TASK3equalIsNot.java
+++ src/TASK3equalIsNot.java
@@ -3,6 +3,10 @@ public class TASK3equalIsNot {
 
     }
     public static boolean equalIsNot(String str){
+        for (int i = 0 ;i <str.length();i++){
+            if (str.charAt(i)=='i'&&str.charAt(i+1)=='s'){
+
+            }
 
     }
 }
