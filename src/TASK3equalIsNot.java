72f195f681f131e5bc46a3a62b9c1e32448c7186
Shahd <shahad9mubarak@gmail.com>
2026-04-21 15:18:16 +0400

    main function

--- src/TASK3equalIsNot.java
+++ src/TASK3equalIsNot.java
@@ -1,20 +1,24 @@
 public class TASK3equalIsNot {
     static void main(String[] args) {
+        String str="This is not";
+        System.out.println();
 
     }
-    public static boolean equalIsNot(String str){
-        int countis=0;
-        int countnot=0;
-        for (int i = 0 ;i <str.length();i++){
-            if (str.substring(i,i+2).equals("is")){
+
+    public static boolean equalIsNot(String str) {
+        int countis = 0;
+        int countnot = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.substring(i, i + 2).equals("is")) {
                 countis++;
 
             }
-            if (str.substring(i,i+3).equals("not")){
+            if (str.substring(i, i + 3).equals("not")) {
                 countnot++;
 
             }
 
+        }
+        return countis == countnot;
     }
-        return countis==countnot;
 }
