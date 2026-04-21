2dcd8f39a5fb2ed26965f048f138fde31f37ba3b
Shahd <shahad9mubarak@gmail.com>
2026-04-21 15:51:59 +0400

    add loop

--- src/TASK5countTriple.java
+++ src/TASK5countTriple.java
@@ -1,13 +1,14 @@
 public class TASK5countTriple {
     static void main(String[] args) {
         String str="abcXXXabc";
+        System.out.println(countTriple(str));
 
 
     }
     public static Integer countTriple(String str){
         int count3=0;
         for (int i =0 ; i <str.length();i++){
-            if (str.substring(i , i+4).equals(i)){
+            if (i<str.length()-3 && str.substring(i , i+4).equals(i)){
                 count3++;
             }
         }
