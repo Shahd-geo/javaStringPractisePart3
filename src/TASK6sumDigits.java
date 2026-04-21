ef0b45444867c91ae12e0461a00fe8c433251be3
Shahd <shahad9mubarak@gmail.com>
2026-04-21 16:05:13 +0400

    add loop

--- src/TASK6sumDigits.java
+++ src/TASK6sumDigits.java
@@ -1,12 +1,14 @@
 public class TASK6sumDigits {
     static void main(String[] args) {
+        String str="aa1bc2d3";
+
 
     }
     public static Integer sumDigits(String str){
         int count = 0;
         for (int i = 0 ; i<=str.length()-1;i++){
             if (Character.isDigit(str)){
-                count++
+                count++;
 
             }
         }
