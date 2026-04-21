4b1e7eb91d2e213de0b12236a97953852a501e0c
Shahd <shahad9mubarak@gmail.com>
2026-04-21 15:35:27 +0400

    task 4

--- src/TASK4gHappy.java
+++ src/TASK4gHappy.java
@@ -1,20 +1,21 @@
 public class TASK4gHappy {
     static void main(String[] args) {
-        String str="xxggxx";
+        String str = "xxggxx";
         System.out.println(gHappy(str));
     }
 
 
-    public static boolean gHappy(String str ){
-        for (int i = 0 ; i<str.length();i++){
-            if (str.charAt(i)=='g' &&str.charAt(i+1)=='g'||str.charAt(i-1)=='g'){
+    public static boolean gHappy(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'g' && str.charAt(i + 1) == 'g' || str.charAt(i - 1) == 'g') {
                 return true;
 
-            }else{
-            return false;
+            } else {
+                return false;
+            }
+
         }
-
+            return true ;
     }
-
 }
 
