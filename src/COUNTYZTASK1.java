ca27849de979372324d8f9ecc58dc0303110c07a
Shahd <shahad9mubarak@gmail.com>
2026-04-21 14:38:05 +0400

    add loop

--- src/COUNTYZTASK1.java
+++ src/COUNTYZTASK1.java
@@ -1,12 +1,16 @@
 public class COUNTYZTASK1 {
     static void main(String[] args) {
-        String countYZ="fez day";
-        for (int i = 0 ; i<countYZ.length();i++){
-            int count = 0;
-            if (i=="z"||"Z"&&(i+1)==""){
-                count+=;
-            } else if () {
-                
+        String countYZ = "fez day";
+        int count = 0;
+        for (int i = 0; i < countYZ.length(); i++) {
+            if (countYZ.charAt(i)=="z" || "Z" && (i + 1) == "") {
+                count +=;
+            } else if (i == "f" || "F" && (i + 1) == "") {
+                count +=;
             }
 
         }
+        return count;
+
+    }
+}
