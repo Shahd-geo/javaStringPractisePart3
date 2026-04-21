46c31111e8a0e9afc6fb2160544a7dc2a1c2f5ef
Shahd <shahad9mubarak@gmail.com>
2026-04-21 14:34:01 +0400

    add string var

---
+++ src/COUNTYZTASK1.java
@@ -0,0 +1,12 @@
+public class COUNTYZTASK1 {
+    static void main(String[] args) {
+        String countYZ="fez day";
+        for (int i = 0 ; i<countYZ.length();i++){
+            int count = 0;
+            if (i=="z"||"Z"&&(i+1)==""){
+                count+=;
+            } else if () {
+                
+            }
+
+        }
