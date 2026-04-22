fb10059c04951f7dec2d572c08375dbde80fed51
Shahd <shahad9mubarak@gmail.com>
2026-04-22 08:27:13 +0400

    task7

--- src/TASK7sameEnds.java
+++ src/TASK7sameEnds.java
@@ -1,6 +1,8 @@
 public class TASK7sameEnds {
     static void main(String[] args) {
-        String sameEnds="abXYab";
-        int max = sameEnds.length()/2;
-        for
+
+            
+        }
+
     }
+
