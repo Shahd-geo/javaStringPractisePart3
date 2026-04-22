814c4e1d4f29b74df35ddd94139a74e73be88778
Shahd <shahad9mubarak@gmail.com>
2026-04-22 09:21:40 +0400

    add count var

--- src/TASK9maxBlock.java
+++ src/TASK9maxBlock.java
@@ -5,7 +5,7 @@ public class TASK9maxBlock {
     public static Integer maxBlock(String str){
         for (int i =0;i<str.length();i++){
             int max=0;
-           // int count=1;
+            int count=1;
 
 
         }
