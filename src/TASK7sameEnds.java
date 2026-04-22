7360c6c584fb9910c1609b8647a5e3a3b1a4aa33
Shahd <shahad9mubarak@gmail.com>
2026-04-22 08:34:55 +0400

    addd result var

--- src/TASK7sameEnds.java
+++ src/TASK7sameEnds.java
@@ -1,20 +1,22 @@
 public class TASK7sameEnds {
     static void main(String[] args) {
 
-            
-        }
-        public static String sameEnds(String str){
+
+    }
+
+    public static String sameEnds(String str) {
         String len = str.length();
-        String result="";
-        for (int i = 0 ; i <=len /2;i++){
-            String start=str.substring(0,i);
-            String end =str.substring(len,-i);
-            if (start.equals(end)){
-                result=start;
+        String result = "";
+        for (int i = 0; i <= len / 2; i++) {
+            String start = str.substring(0, i);
+            String end = str.substring(len, -i);
+            if (start.equals(end)) {
+                result = start;
             }
 
         }
-        }
+        return result;
 
     }
+}
 
