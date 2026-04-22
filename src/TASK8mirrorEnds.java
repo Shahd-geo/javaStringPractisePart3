3c43d0c88c7f99e5287180be5e641bd7a847012b
Shahd <shahad9mubarak@gmail.com>
2026-04-22 09:05:33 +0400

    add if con

--- src/TASK8mirrorEnds.java
+++ src/TASK8mirrorEnds.java
@@ -7,9 +7,12 @@ public class TASK8mirrorEnds {
         for (int i = 0 ;i<str.length();i++){
         if (str.charAt(i)==str.charAt(str.length()-1 -i)){
             result+=str.charAt(i);
+        }else {
+            break;
         }
 
 
         }
+        r
     }
 }
