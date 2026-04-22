57066b556492178957215b0c92209f819b75f856
Shahd <shahad9mubarak@gmail.com>
2026-04-22 15:03:11 +0400

    task 1 :completed

--- src/COUNTYZTASK1.java
+++ src/COUNTYZTASK1.java
@@ -1,17 +1,29 @@
 public class COUNTYZTASK1 {
+    /* static void main(String[] args) {
+         String countYZ = "day fez";
+
+         int count = 0;
+         for (int i = 0; i < countYZ.length(); i++) {
+             if (countYZ.charAt(i)=='z'|| countYZ.charAt(i)=='Z' ||
+                     countYZ.charAt(i)=='y'||countYZ.charAt(i)=='Y' &&
+                     (i==countYZ.length()-1||!Character.isLetter(countYZ.charAt(i+1)))){
+                 count ++;
+             }
+
+         }
+         System.out.println(count);
+
+     }
+ }
+     */
     static void main(String[] args) {
-        String countYZ = "day fez";
-
+        String str = "day fez";
         int count = 0;
-        for (int i = 0; i < countYZ.length(); i++) {
-            if (countYZ.charAt(i)=='z'|| countYZ.charAt(i)=='Z' ||
-                    countYZ.charAt(i)=='y'||countYZ.charAt(i)=='Y' &&
-                    (i==countYZ.length()-1||!Character.isLetter(countYZ.charAt(i+1)))){
-                count ++;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'z' || str.charAt(i) == 'Z' || str.charAt(i) == 'y' || str.charAt(i) == 'Y' && (i == str.length() - 1 || !Character.isLetter(str.charAt(i + 1)))) {
+                count++;
             }
-
         }
         System.out.println(count);
-
     }
 }
