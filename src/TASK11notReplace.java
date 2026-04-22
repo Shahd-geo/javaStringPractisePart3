e914145d729cc8c58a25211512bcf106ad043b0d
Shahd <shahad9mubarak@gmail.com>
2026-04-22 10:23:17 +0400

    TASK11

--- src/TASK11notReplace.java
+++ src/TASK11notReplace.java
@@ -3,6 +3,8 @@ public class TASK11notReplace {
 
     }
     public static String notReplace(String str){
-        ()
+        for (int i = 0 ; i<str.length();i++){
+            if (str.substring(i , i+2).equals("is")&&str.subst)
+        }
     }
 }
