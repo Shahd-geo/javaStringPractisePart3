0b0e2f63a9df756b4ad50c4af9ab4a4378a53e99
Shahd <shahad9mubarak@gmail.com>
2026-04-22 09:29:50 +0400

    return max

--- src/TASK9maxBlock.java
+++ src/TASK9maxBlock.java
@@ -1,28 +1,31 @@
 public class TASK9maxBlock {
     static void main(String[] args) {
-        String str="hoopla";
-        System.out.println(maxBlock());
+        String str = "hoopla";
+        System.out.println(maxBlock(str));
 
 
     }
-    public static Integer maxBlock(String str){
-        int max=0;
-        int count=1;
-        for (int i =0;i<str.length();i++){
 
-            if (i<str.length()-1 &&str.charAt(i)==str.charAt(i +1)){
+    public static Integer maxBlock(String str) {
+        int max = 0;
+        int count = 1;
+        for (int i = 0; i < str.length(); i++) {
+
+            if (i < str.length() - 1 && str.charAt(i) == str.charAt(i + 1)) {
                 count++;
-            }else {
-                if (count<max){
-                    max=count;
+            } else {
+                if (count < max) {
+                    max = count;
                 }
-                count=1;
+                count = 1;
             }
-            return max;
+
 
 
         }
+        return max;
 
     }
+}
 
 }
