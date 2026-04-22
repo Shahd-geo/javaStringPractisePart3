6c509f55e94a0df6a36acc27608406fccff65134
Shahd <shahad9mubarak@gmail.com>
2026-04-22 10:33:05 +0400

    make sure not have any char after is

--- src/TASK11notReplace.java
+++ src/TASK11notReplace.java
@@ -7,7 +7,10 @@ public class TASK11notReplace {
         for (int i = 0 ; i<str.length();i++){
             if (i<str.length()&&str.substring(i , i+2).equals("is") &&
                     i==0 || !Character.isLetter(str.charAt(i-1)) ||
-                    i+2>=str.length()||!Character.isLetter(str.charAt(i+2))
+                    i+2>=str.length()||!Character.isLetter(str.charAt(i+2))){
+                result+="is not "
+
+            }
         }
     }
 }
