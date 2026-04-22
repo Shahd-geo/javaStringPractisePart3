823a4b4e1d4f9da03501a30559fff1c20d9070de
Shahd <shahad9mubarak@gmail.com>
2026-04-22 15:10:31 +0400

    task 2 solve

--- src/TASK2withoutString.java
+++ src/TASK2withoutString.java
@@ -1,36 +1,44 @@
 public class TASK2withoutString {
-   /* static void main(String[] args) {
+    /* static void main(String[] args) {
+         String str1= "Hello there";
+         String remove="llo";
+         System.out.println(withoutString(str1,remove));
+
+
+     }
+     public static String withoutString(String str1,String remove){
+         String result = "";
+         String str=str1.toLowerCase();
+         String Remove=remove.toLowerCase();
+         for (int i =0 ;i<str1.length();i++){
+             if (str1.startsWith(remove,i)){
+                 i+=remove.length()-1;//skip out range
+             }else {
+                 result+=str1.charAt(i);
+             }
+         }
+         return result;
+     }
+ }
+ */
+    static void main(String[] args) {
         String str1= "Hello there";
         String remove="llo";
         System.out.println(withoutString(str1,remove));
 
 
     }
-    public static String withoutString(String str1,String remove){
+
+    public static String withoutString(String str1, String remove) {
         String result = "";
-        String str=str1.toLowerCase();
-        String Remove=remove.toLowerCase();
-        for (int i =0 ;i<str1.length();i++){
-            if (str1.startsWith(remove,i)){
-                i+=remove.length()-1;//skip out range
-            }else {
-                result+=str1.charAt(i);
+        for (int i = 0; i < str1.length(); i++) {
+            if (str1.startsWith(remove, i)) {
+                i += remove.length() - 1;
+
+            } else {
+                result += str1.charAt(i);
             }
         }
         return result;
     }
 }
-*/
-   static void main(String[] args) {
-
-   }
-
-    public static String withoutString(String str1,String remove){
-       String result = "";
-       for (int i =0 ; i <str1.length();i++){
-           if (str1.startsWith(remove,i)){
-               i+=remove.length()-1;
-
-           }
-       }
-    }
