2a70bc422acaba930350f1070b6cdff29581a2d0
Shahd <shahad9mubarak@gmail.com>
2026-04-22 16:13:13 +0400

    task 3

--- src/TASK3equalIsNot.java
+++ src/TASK3equalIsNot.java
@@ -1,49 +1,46 @@
 public class TASK3equalIsNot {
-   /* static void main(String[] args) {
+    /* static void main(String[] args) {
+         String str = "This is not";
+         System.out.println(equalIsNot(str));
+
+     }
+
+     public static boolean equalIsNot(String str) {
+         int countis = 0;
+         int countnot = 0;
+         for (int i = 0; i < str.length(); i++) {
+             if (i <=str.length()-2 &&
+                     str.substring(i, i + 2).equals("is")) {
+                 countis++;
+
+             }
+             if (i <=str.length()-3 &&
+                     str.substring(i, i + 3).equals("not")) {
+                 countnot++;
+
+             }
+         }
+
+         return countis == countnot;
+     }
+ }
+ */
+    static void main(String[] args) {
         String str = "This is not";
         System.out.println(equalIsNot(str));
-
     }
 
     public static boolean equalIsNot(String str) {
         int countis = 0;
         int countnot = 0;
         for (int i = 0; i < str.length(); i++) {
-            if (i <=str.length()-2 &&
-                    str.substring(i, i + 2).equals("is")) {
+            if (str.substring(i, i + 2).equals("is")) {
                 countis++;
-
-            }
-            if (i <=str.length()-3 &&
-                    str.substring(i, i + 3).equals("not")) {
+            } else if (str.substring(i, i + 3).equals("not")) {
                 countnot++;
 
             }
         }
-
-        return countis == countnot;
+        return countis==countnot;
     }
 }
-*/
-   static void main(String[] args) {
-       String str = "This is not";
-       System.out.println(equalIsNot(str));
-   }
-
-    public static boolean equalIsNot(String str) {
-       int countis=0;
-       int countnot=0;
-       for (int i = 0 ; i<str.length();i++){
-           if (str.substring(i,i+2).equals("is")){
-               countis++;
-           } else if (str.substring(i,i+3).equals("not")) {
-               countnot++;
-
-           }
-       }if (countis==countnot){
-           return true;
-        }
-       return false
-    }
-
-   }
