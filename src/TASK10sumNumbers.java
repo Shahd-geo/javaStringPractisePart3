907989244246d0fda0e048de22279d3e2ea825e4
Shahd <shahad9mubarak@gmail.com>
2026-04-22 12:26:14 +0400

    Task 10 : after corrected

--- src/TASK10sumNumbers.java
+++ src/TASK10sumNumbers.java
@@ -1,29 +1,25 @@
 public class TASK10sumNumbers {
     static void main(String[] args) {
-        String str="abc123xyz";
+        String str = "abc123xyz";
         System.out.println(sumNumbers(str));
 
     }
+
     public static Integer sumNumbers(String str) {
         String num = "";
         int sum = 0;
         for (int i = 0; i < str.length(); i++) {
             if (Character.isDigit(str.charAt(i))) {
                 num += str.charAt(i);
-
-            }else {
-                if (!num.equals("")) {
+                if (i == str.length() - 1 || !Character.isDigit(str.charAt(i+1))) {
                     sum += Integer.parseInt(num);
                     num = "";
+
                 }
             }
-        }
-        if (!num.equals("")) {
-            sum += Integer.parseInt(num);
         }
 
         return sum;
     }
+
 }
-
-
--- src/TASK11notReplace.java
+++ src/TASK11notReplace.java
@@ -1,18 +1,22 @@
 public class TASK11notReplace {
     static void main(String[] args) {
+        String str="is test";
+
 
     }
-    public static String notReplace(String str){
-        String result="";
-        for (int i = 0 ; i<str.length();i++){
-            if (i<str.length()&&str.substring(i , i+2).equals("is") &&
-                    i==0 || !Character.isLetter(str.charAt(i-1)) ||
-                    i+2>=str.length()||!Character.isLetter(str.charAt(i+2))){
-                result+="is not ";
+    public static String notReplace(String str) {
+        String result = "";
+        for (int i = 0; i < str.length(); i++) {
+            if (i < str.length() && str.substring(i, i + 2).equals("is") &&
+                    i == 0 || !Character.isLetter(str.charAt(i - 1)) ||
+                    i + 2 >= str.length() || !Character.isLetter(str.charAt(i + 2))) {
+                result += "is not ";
 
-            }else {
-                result+=str.charAt(i);
+            } else {
+                result += str.charAt(i);
             }
         }
+        return result;
+
     }
 }
