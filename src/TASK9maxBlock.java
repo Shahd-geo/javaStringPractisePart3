849e1d0e85f3a24dc86f3460d8076d8b82b53b0a
Shahd <shahad9mubarak@gmail.com>
2026-04-22 09:21:24 +0400

    add max var

--- src/TASK9maxBlock.java
+++ src/TASK9maxBlock.java
@@ -4,6 +4,9 @@ public class TASK9maxBlock {
     }
     public static Integer maxBlock(String str){
         for (int i =0;i<str.length();i++){
+            int max=0;
+           // int count=1;
+
 
         }
 
