6b94c9feba7d8977c44ca0d4d8988602e7549f38
Shahd <shahad9mubarak@gmail.com>
2026-04-22 15:09:31 +0400

    task 2 solve

--- src/TASK2withoutString.java
+++ src/TASK2withoutString.java
@@ -1,5 +1,5 @@
 public class TASK2withoutString {
-    static void main(String[] args) {
+   /* static void main(String[] args) {
         String str1= "Hello there";
         String remove="llo";
         System.out.println(withoutString(str1,remove));
@@ -20,3 +20,17 @@ public class TASK2withoutString {
         return result;
     }
 }
+*/
+   static void main(String[] args) {
+
+   }
+
+    public static String withoutString(String str1,String remove){
+       String result = "";
+       for (int i =0 ; i <str1.length();i++){
+           if (str1.startsWith(remove,i)){
+               i+=remove.length()-1;
+
+           }
+       }
+    }
