b21950a8abb47da1e30bda83082755020b405555
Shahd <shahad9mubarak@gmail.com>
2026-04-22 09:59:15 +0400

    add return

--- src/TASK10sumNumbers.java
+++ src/TASK10sumNumbers.java
@@ -2,16 +2,26 @@ public class TASK10sumNumbers {
     static void main(String[] args) {
 
     }
-    public static Integer sumNumbers(String str){
+    public static Integer sumNumbers(String str) {
         String num = "";
-        int sum =0;
-        for (int i = 0 ; i<str.length();i++){
-            if (Character.isDigit(i)){
-                num+=;
+        int sum = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isDigit(str.charAt(i))) {
+                num += str.charAt(i);
 
             }
+        } else{
+            if (!num.equals("")) {
+                sum += Integer.parseInt(num);
+                num = "";
+            }
+        }
+        if (!num.equals("")) {
+            sum += Integer.parseInt(num);
+        }
 
-        }sum+=Integer.parseInt(num);
-
+        return sum;
     }
 }
+
+
